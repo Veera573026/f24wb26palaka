6d1ed051a01b98a29f050d5ba75d97e49ac90393
Veera573026 <veeranjaneyulu704@gmail.com>
2024-11-05 17:38:11 -0600

    added new end point

--- routes/pick.js
+++ routes/pick.js
@@ -1,8 +1,24 @@
-var express = require('express');
-var router = express.Router();
+// pick.js
 
-router.get('/', (req, res) => {
-  res.render('randomitem', { title: "A random item" });
+const express = require('express');
+const app = express();
+
+// Set the view engine to pug
+app.set('view engine', 'pug');
+app.set('views', './views'); // Adjust this path based on your project
+
+// Route for random item
+app.get('/randomitem', (req, res) => {
+  // Render randomitem.pug
+  res.render('randomitem', {
+    image_names: [
+      'item1.jpg',
+      'item2.jpg',
+      'item3.jpg',
+      'item4.jpg',
+      'item5.jpg'
+    ]
+  });
 });
 
-module.exports = router;
+// Other routes...
