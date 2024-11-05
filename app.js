64d6042979b383131291f65c8141432d62a2cbff
Veera573026 <veeranjaneyulu704@gmail.com>
2024-11-05 17:44:14 -0600

    updated

--- app.js
+++ app.js
@@ -1,24 +1,31 @@
-// app.js
-
+var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var createError = require('http-errors');
 
-// Import the routers
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var pickRouter = require('./routes/pick'); // Import pick.js
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+app.get('/grid', (req, res) => {
+  let query = req.query;
+  console.log(`rows: ${query.rows}`);
+  console.log(`cols: ${query.cols}`);
+  
+  res.render('grid', { title: 'Grid Display', query: query });
+});
 
-// Use the pickRouter for the '/pick' route
-app.use('/pick', pickRouter); // Fixed: Removed the extra 's'
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
@@ -42,9 +49,11 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
+  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
