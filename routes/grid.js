8cf4a9563bf635bd0a7ab1755c5d907ebd4fe343
Veera573026 <veeranjaneyulu704@gmail.com>
2024-11-05 16:48:22 -0600

    added new end point

--- routes/grid.js
+++ routes/grid.js
@@ -1,25 +1,11 @@
-const express = require('express');
-const app = express();
-const path = require('path');
+var express = require('express');
+var router = express.Router();
 
-// Set Pug as the view engine
-app.set('view engine', 'pug');
-app.set('views', path.join(__dirname, 'views'));
-
-// Route for grid page
-app.get('/grid', (req, res) => {
-  // Extract query parameters
+router.get('/', (req, res) => {
   let query = req.query;
-
-  // Log the query values (for debugging)
   console.log(`rows ${query.rows}`);
   console.log(`cols ${query.cols}`);
-
-  // Render the grid.pug template and pass the query data
   res.render('grid', { title: "Grid Display", query: query });
 });
 
-// Start the server
-app.listen(3000, () => {
-  console.log('Server running on http://localhost:3000');
-});
+module.exports = router;
