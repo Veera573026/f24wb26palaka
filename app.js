2af0f1ea67305b3acfa7f91501a530b98881381f
Veera573026 <veeranjaneyulu704@gmail.com>
2024-11-05 17:26:10 -0600

    added new end point

--- app.js
+++ app.js
@@ -1,31 +1,26 @@
-var createError = require('http-errors');
+// app.js
+
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var createError = require('http-errors');
 
+// Import the routers
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var pickRouter = require('./routes/pick'); // Import pick.js
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-app.get('/grid', (req, res) => {
-  let query = req.query;
-  console.log(`rows: ${query.rows}`);
-  console.log(`cols: ${query.cols}`);
-  
-  res.render('grid', { title: 'Grid Display', query: query });
-});
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// Use the pickRouter for the '/pick' route
+app.use('/pick', pickRouter);
 
+// Other routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
@@ -49,11 +44,9 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
