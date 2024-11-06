ab99523f32760a9341c8e404b368f114221ff92c
Veera573026 <veeranjaneyulu704@gmail.com>
2024-11-05 18:27:30 -0600

    updated

--- routes/plants.js
+++ routes/plants.js
@@ -1,25 +1,18 @@
 const express = require('express');
-const app = express();
-const path = require('path');
+const router = express.Router();
 
-// Set Pug as the view engine
-app.set('view engine', 'pug');
-app.set('views', path.join(__dirname, 'views'));
-
-// Route to display plants page
-app.get('/plants', (req, res) => {
-  // Example plant data to pass to the view
-  const results = [
+// Sample data for artifacts
+const results = [
     { plant_name: "Cactus", plant_type: "Succulent", plant_age: 5 },
     { plant_name: "Rose", plant_type: "Flower", plant_age: 2 },
     { plant_name: "Oak Tree", plant_type: "Tree", plant_age: 50 }
   ];
-  
-  // Pass the 'results' array to the plants.pug template
-  res.render('plants', { results: results });
+
+router.get('/', (req, res) => {
+  res.render('plants', { 
+    title: 'Plants', 
+    results: plants 
+  });
 });
 
-// Start the server
-app.listen(3000, () => {
-  console.log('Server running on http://localhost:3000');
-});
+module.exports = router;
