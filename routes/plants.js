222466a2f6ad69d3badc4dc15c5ab197cf9d4b2d
Veera573026 <veeranjaneyulu704@gmail.com>
2024-11-05 18:30:55 -0600

    updated

--- routes/plants.js
+++ routes/plants.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const router = express.Router();
 
-// Sample data for artifacts
+// Sample data for plants
 const results = [
-    { plant_name: "Cactus", plant_type: "Succulent", plant_age: 5 },
-    { plant_name: "Rose", plant_type: "Flower", plant_age: 2 },
-    { plant_name: "Oak Tree", plant_type: "Tree", plant_age: 50 }
-  ];
+  { plant_name: "Cactus", plant_type: "Succulent", plant_age: 5 },
+  { plant_name: "Rose", plant_type: "Flower", plant_age: 2 },
+  { plant_name: "Oak Tree", plant_type: "Tree", plant_age: 50 }
+];
 
+// Route to render the plants page
 router.get('/', (req, res) => {
   res.render('plants', { 
     title: 'Plants', 
-    results: plants 
+    results: results  // Passing 'results' to the Pug template
   });
 });
 
